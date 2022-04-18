414123fddc9f1ac65bb60dff18a3fad23fc23fba
craig <craigparra@gmail>
2022-04-19 09:27:14 +1000

    coverage

--- main_test.go
+++ main_test.go
@@ -1,6 +1,27 @@
 package main
 
-import "testing"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	// call flag.Parse() here if TestMain uses flags
+	rc := m.Run()
+
+	// rc 0 means we've passed,
+	// and CoverMode will be non empty if run with -cover
+	if rc == 0 && testing.CoverMode() != "" {
+		c := testing.Coverage()
+		if c < 0.75 {
+			fmt.Println("Tests passed but coverage failed at", c)
+			rc = -1
+		}
+	}
+	os.Exit(rc)
+}
 
 func TestEncryptDecrypt(t *testing.T) {
 
@@ -16,4 +37,47 @@ func TestEncryptDecrypt(t *testing.T) {
 	if err == nil {
 		t.Errorf("err == nil: err is:%s", "nil")
 	}
+}
+
+func TestMainFunc(t *testing.T) {
+
+	fmt.Println(os.Args)
+	origArgs := os.Args
+	defer func() { os.Args = origArgs }()
+
+	args := make([]string, 0)
+	args = append(args, "gosypt")
+
+	flag.CommandLine = flag.NewFlagSet("0", flag.ContinueOnError)
+	os.Args = append(args, "-v")
+	main()
+
+	flag.CommandLine = flag.NewFlagSet("1", flag.ContinueOnError)
+	os.Args = append(args, "-h")
+	main()
+
+	flag.CommandLine = flag.NewFlagSet("2", flag.ContinueOnError)
+	os.Args = append(args, "-p")
+	main()
+
+	flag.CommandLine = flag.NewFlagSet("3", flag.ContinueOnError)
+	os.Args = append(args, "-p", "1234567890123456")
+	main()
+
+	flag.CommandLine = flag.NewFlagSet("4", flag.ContinueOnError)
+	os.Args = append(args, "-p", "1234567890123456", "-e", "HelloWorld")
+	main()
+
+	flag.CommandLine = flag.NewFlagSet("5", flag.ContinueOnError)
+	os.Args = append(args, "-p", "1234567890123456", "-d", "G4hvsS/rhncB4cO58UEhz9crxQ9vp93k83l8mlBkz+c=")
+	main()
+
+	flag.CommandLine = flag.NewFlagSet("6", flag.ContinueOnError)
+	os.Args = append(args, "-p", "1234567890123456", "-d")
+	main()
+
+	flag.CommandLine = flag.NewFlagSet("7", flag.ContinueOnError)
+	os.Args = append(args, "-p", "1234567890123450", "-e", "HelloWorld")
+	main()
+
 }
